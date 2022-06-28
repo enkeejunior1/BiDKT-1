9ec36cc6f270d0f9dbe86edfef16005eac8d55c5
codingchild <codingchild@gmail.com>
2022-06-28 14:52:10 +0000

    new learning

--- src/train.sh
+++ src/train.sh
@@ -1,60 +1,42 @@
 #!/bin/bash
 
-python \
-train.py \
---model_fn model.pth \
---dataset_name assist2015 \
---num_encoder 8 \
---crit binary_cross_entropy \
---batch_size 256 \
---grad_acc True \
---grad_acc_iter 2 \
---fivefold False \
---n_epochs 1000
-
-python \
-train.py \
---model_fn model.pth \
---dataset_name assist2015 \
---num_encoder 12 \
---crit binary_cross_entropy \
---batch_size 256 \
---grad_acc True \
---grad_acc_iter 2 \
---fivefold False \
---leakyrelu False \
---n_epochs 1000
-
-datasets1="assist2017 statics slepemapy"
-
-for dataset1 in ${datasets1}
-do
-    python \
-    train.py \
-    --model_fn model.pth \
-    --dataset_name ${dataset1} \
-    --num_encoder 12 \
-    --crit binary_cross_entropy \
-    --batch_size 256 \
-    --grad_acc True \
-    --grad_acc_iter 2 \
-    --fivefold True \
-    --n_epochs 1000
-done
-
-datasets2="assist2015 assist2009 assist2017 algebra2005 algebra2006 statics slepemapy"
+datasets2="assist2009 assist2015 assist2017"
+encoder_nums="12 16"
 
 for dataset2 in ${datasets2}
 do
-    python \
-    train.py \
-    --model_fn model.pth \
-    --dataset_name ${dataset2} \
-    --num_encoder 12 \
-    --crit rmse \
-    --batch_size 256 \
-    --grad_acc True \
-    --grad_acc_iter 2 \
-    --fivefold True \
-    --n_epochs 1000
+    for encoder_num in ${encoder_nums}
+    do
+        python \
+        train.py \
+        --model_fn model.pth \
+        --dataset_name ${dataset2} \
+        --num_encoder ${encoder_num} \
+        --crit rmse \
+        --batch_size 256 \
+        --grad_acc True \
+        --grad_acc_iter 2 \
+        --use_leakyrelu True \
+        --fivefold True \
+        --n_epochs 1000
+    done
+done
+
+for dataset2 in ${datasets2}
+do
+    for encoder_num in ${encoder_nums}
+    do
+        python \
+        train.py \
+        --model_fn model.pth \
+        --dataset_name ${dataset2} \
+        --num_encoder ${encoder_num} \
+        --crit rmse \
+        --batch_size 256 \
+        --grad_acc True \
+        --grad_acc_iter 2 \
+        --use_leakyrelu False \
+        --fivefold True \
+        --n_epochs 1000
+    done
 done
