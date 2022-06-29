85c6940c924ed9764fff3a887d13e20aa4124dce
codingchild <codingchild@gmail.com>
2022-06-29 11:04:40 +0000

    change train.sh

--- src/train.sh
+++ src/train.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
-
-pid_datasets="assist2009_pid assist2017_pid"
-datasets="assist2009 assist2015 assist2017"
 encoder_nums="12 16"
 
-for pid_dataset in ${pid_datasets}
+for encoder_num in ${encoder_nums}
 do
-    for encoder_num in ${encoder_nums}
-    do
-        python \
-        train.py \
-        --model_fn bert4kt_plus_leakyrelu_model.pth \
-        --model_name bert4kt_plus \
-        --dataset_name ${pid_dataset} \
-        --num_encoder ${encoder_num} \
-        --batch_size 256 \
-        --grad_acc True \
-        --grad_acc_iter 2 \
-        --use_leakyrelu True \
-        --fivefold True \
-        --n_epochs 1000
-    done
+    python \
+    train.py \
+    --model_fn bert4kt_plus_leakyrelu_model.pth \
+    --model_name bert4kt_plus \
+    --dataset_name assist2017_pid \
+    --num_encoder ${encoder_num} \
+    --batch_size 256 \
+    --grad_acc True \
+    --grad_acc_iter 2 \
+    --use_leakyrelu True \
+    --fivefold True \
+    --n_epochs 1000
 done
+
+datasets="assist2009 assist2015 assist2017"
 
 #gelu
 for dataset in ${datasets}
