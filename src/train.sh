805e26b6351d72032b5cb5baa6feda08fc506b05
codingchild <codingchild@gmail.com>
2022-07-02 07:19:47 +0000

    change

--- src/train.sh
+++ src/train.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
-encoder_nums="12 16"
-
 pid_datasets="assist2009_pid assist2017_pid algebra2005_pid algebra2006_pid slepemapy_pid"
 
 for pid_dataset in ${pid_datasets}
 do
-    for encoder_num in ${encoder_nums}
-    do
-        python \
-        train.py \
-        --model_fn bert4kt_plus_leakyrelu_model.pth \
-        --model_name bert4kt_plus \
-        --dataset_name ${pid_dataset} \
-        --num_encoder ${encoder_num} \
-        --batch_size 256 \
-        --grad_acc True \
-        --grad_acc_iter 2 \
-        --use_leakyrelu True \
-        --fivefold True \
-        --n_epochs 1000
-    done
+    python \
+    train.py \
+    --model_fn model.pth \
+    --model_name ma_bert4kt_plus \
+    --dataset_name ${pid_dataset} \
+    --num_encoder 12 \
+    --batch_size 128 \
+    --grad_acc True \
+    --grad_acc_iter 4 \
+    --use_leakyrelu True \
+    --fivefold True \
+    --n_epochs 1000
 done
