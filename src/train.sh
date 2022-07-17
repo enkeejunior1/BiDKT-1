7f1bd984104456bef3f6d9294ed7ec71465232b8
codingchild <codingchild@gmail.com>
2022-07-17 21:32:38 +0000

    add

--- src/train.sh
+++ src/train.sh
@@ -1,15 +1,49 @@
 #!/bin/bash
-dataset_names="assist2009_pid_diff_pt"
 
-for dataset_name in ${dataset_names}
+assist_dataset_names="assist2009_pid_diff assist2012_pid_diff assist2017_pid_diff"
+algebra_dataset_names="algebra2005_pid_diff algebra2006_pid_diff"
+algebra_dataset_names="algebra2005_pid_diff algebra2006_pid_diff"
+
+python \
+train.py \
+--model_fn monaconvbert4kt_plus_diff_auc.pth \
+--model_name monaconvbert4kt_plus_diff \
+--dataset_name assist2017_pid_diff \
+--num_encoder 12 \
+--batch_size 256 \
+--grad_acc True \
+--grad_acc_iter 2 \
+--use_leakyrelu True \
+--fivefold True \
+--n_epochs 1000
+
+
+
+for algebra_dataset_name in ${algebra_dataset_names}
+do
+    python \
+    train.py \
+    --model_fn monaconvbert4kt_plus_diff_auc.pth \
+    --model_name monaconvbert4kt_plus_diff \
+    --dataset_name ${algebra_dataset_name} \
+    --num_encoder 12 \
+    --batch_size 128 \
+    --grad_acc True \
+    --grad_acc_iter 4 \
+    --use_leakyrelu True \
+    --fivefold True \
+    --n_epochs 1000
+done
+
+for assist_dataset_name in ${assist_dataset_names}
 do
     python \
     train.py \
     --model_fn monaconvbert4kt_plus_diff_rmse.pth \
     --model_name monaconvbert4kt_plus_diff \
-    --dataset_name ${dataset_name} \
+    --dataset_name ${assist_dataset_name} \
     --num_encoder 12 \
-    --batch_size 64 \
+    --batch_size 256 \
     --grad_acc True \
     --grad_acc_iter 2 \
     --use_leakyrelu True \
@@ -18,13 +52,82 @@ do
     --n_epochs 1000
 done
 
-for algebra_dataset in ${algebra_dataset_names}
+for algebra_dataset_name in ${algebra_dataset_names}
 do
     python \
     train.py \
     --model_fn monaconvbert4kt_plus_diff_rmse.pth \
     --model_name monaconvbert4kt_plus_diff \
-    --dataset_name ${algebra_dataset} \
+    --dataset_name ${algebra_dataset_name} \
+    --num_encoder 12 \
+    --batch_size 128 \
+    --grad_acc True \
+    --grad_acc_iter 4 \
+    --use_leakyrelu True \
+    --crit rmse \
+    --fivefold True \
+    --n_epochs 1000
+done
+
+# no diff
+for assist_dataset_name in ${assist_dataset_names}
+do
+    python \
+    train.py \
+    --model_fn monaconvbert4kt_plus_auc.pth \
+    --model_name monaconvbert4kt_plus \
+    --dataset_name ${assist_dataset_name} \
+    --num_encoder 12 \
+    --batch_size 256 \
+    --grad_acc True \
+    --grad_acc_iter 2 \
+    --use_leakyrelu True \
+    --fivefold True \
+    --n_epochs 1000
+done
+
+algebra_dataset_names="algebra2005_pid_diff algebra2006_pid_diff"
+
+for algebra_dataset_name in ${algebra_dataset_names}
+do
+    python \
+    train.py \
+    --model_fn monaconvbert4kt_plus_auc.pth \
+    --model_name monaconvbert4kt_plus \
+    --dataset_name ${algebra_dataset_name} \
+    --num_encoder 12 \
+    --batch_size 128 \
+    --grad_acc True \
+    --grad_acc_iter 4 \
+    --use_leakyrelu True \
+    --fivefold True \
+    --n_epochs 1000
+done
+
+for assist_dataset_name in ${assist_dataset_names}
+do
+    python \
+    train.py \
+    --model_fn monaconvbert4kt_plus_rmse.pth \
+    --model_name monaconvbert4kt_plus \
+    --dataset_name ${assist_dataset_name} \
+    --num_encoder 12 \
+    --batch_size 256 \
+    --grad_acc True \
+    --grad_acc_iter 2 \
+    --use_leakyrelu True \
+    --crit rmse \
+    --fivefold True \
+    --n_epochs 1000
+done
+
+for algebra_dataset_name in ${algebra_dataset_names}
+do
+    python \
+    train.py \
+    --model_fn monaconvbert4kt_plus_rmse.pth \
+    --model_name monaconvbert4kt_plus \
+    --dataset_name ${algebra_dataset_name} \
     --num_encoder 12 \
     --batch_size 128 \
     --grad_acc True \
